fd629e26a0f930577fddc6447e1c0f40a496bb6d
jexxer <jexxe@live.com>
2021-07-29 10:12:30 -0400

    marvel question obj created

---
+++ script.js
@@ -0,0 +1,53 @@
+const marvelQuestions = {
+    question1: {
+        question: 'How many Infinity Stones are there?',
+        answer: 'Six',
+        options: ['Five', 'Seven', 'Four', 'Six']
+    },
+    question2: {
+        question: 'Where is Captain America from?',
+        answer: 'Brooklyn',
+        options: ['Manhattan', 'Brooklyn', 'Queens', 'Harlem']
+    },
+    question3: {
+        question: 'Peggy Carter has a great-niece named...',
+        answer: 'Sharon Carter',
+        options: ['Sharon Carter', 'Carole Carter', 'Rosalynn Carter', 'Angela Carter']
+    },
+    question4: {
+        question: 'Captain America and Bucky\'s arm are made of...',
+        answer: 'Vibranium',
+        options: ['Titanium', 'Tungsten', 'Vibranium', 'Carbon Steel']
+    },
+    question5: {
+        question: 'In which movie did Spider-Man make his first appearance in the MCU?',
+        answer: 'Captain America: Civil War',
+        options: ['Thor: Ragnarok', 'Avengers: End Game', 'Avengers: Age of Ultron', 'Captain America: Civil War']
+    },
+    question6: {
+        question: 'What species is Groot?',
+        answer: 'Flora colossus',
+        options: ['Phyllostachys Aurora', 'Flora Colossus', 'Ficus Lyrata', 'Phoenix Dactylifera']
+    },
+    question7: {
+        question: 'What color blood do Skrulls have?',
+        answer: 'Blue',
+        options: ['Blue', 'Red', 'Purple', 'Green']
+    },
+    question8: {
+        question: "What is Hawkeye’s weapon of choice?",
+        answer: 'Bow',
+        options: ['Staff', 'Rifle', 'Bow', 'Cross-bow']
+    },
+    question9: {
+        question: 'From which Infinity Stone did Carol Danvers get her powers?',
+        answer: 'Space Stone',
+        options: ['Space Stone', 'Soul Stone', 'Power Stone', 'Mind Stone']
+    },
+    question10: {
+        question: 'What planet is Ronan from?',
+        answer: 'Hala',
+        options: ['Morag', 'Carthan', 'Xandar', 'Hala']
+    }
+}
+
